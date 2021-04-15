dd9a59ff55cb392999cab2203aec8384b0dc45de
Ottonat <ottonat@outlook.com>
2021-04-15 11:41:05 -0300

    Atualizando algumas classes

---
+++ Arquivos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos
+{
+    class Program
+    {
+        // File, FileInfo, IOException
+        static void Main(string[] args)
+        {
+            string Origem = @"C:\Users\Otton\Documents\Arquivos_testes\texto1.txt";
+            string Destino = @"C:\Users\Otton\Documents\Arquivos_testes\texto2.txt";
+
+            try
+            {
+                FileInfo fi = new FileInfo(Origem);
+                fi.CopyTo(Destino);
+                string[] lines = File.ReadAllLines(Origem);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Ocorreu um erro:");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
