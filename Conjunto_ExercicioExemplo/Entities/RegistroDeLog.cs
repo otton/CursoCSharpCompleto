8ca6a8babb9c57e970f764e2b021e2063ef3ceea
Ottonat <ottonat@outlook.com>
2021-05-12 14:07:17 -0300

    Exercicio de fixação envolvendo arquivos e conjuntos

---
+++ Conjunto_ExercicioExemplo/Entities/RegistroDeLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conjunto_ExercicioExemplo.Entities
+{
+    class RegistroDeLog
+    {
+        public string Usuario { get; set; }
+        public DateTime Instante { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Usuario.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is RegistroDeLog))
+            {
+                return false;
+            }
+            RegistroDeLog outro = obj as RegistroDeLog;
+            return Usuario.Equals(outro.Usuario);
+        }
+    }
+}
---
+++ Conjunto_ExercicioExemplo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Conjunto_ExercicioExemplo.Entities;
+
+namespace Conjunto_ExercicioExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<RegistroDeLog> set = new HashSet<RegistroDeLog>();
+
+            Console.Write("Entre com o caminho do arquivo: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader streamReader = File.OpenText(path))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string[] linha = streamReader.ReadLine().Split(' ');
+                        string usuario = linha[0];
+                        DateTime instante = DateTime.Parse(linha[1]);
+                        set.Add(new RegistroDeLog { Usuario = usuario, Instante = instante });
+                    }
+                    Console.WriteLine("Total de usuários: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
